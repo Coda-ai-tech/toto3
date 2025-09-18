c7328ba393bfaeeeb953eed00656e46df0d0637d
Karvis Law <karvisl@stridesolutions.com.au>
2025-09-18 18:14:28 +1000

    revert award page change

--- src/app/[lang]/awards/zh.tsx
+++ src/app/[lang]/awards/zh.tsx
@@ -1,68 +1,7 @@
 'use client';
-import Awards, {AwardList} from '@/app/components/Sections/Awards';
 import useScrollDirection, { ScrollUp } from '@/hook/useScrollDirection';
-import { ButtonLinkElement } from '@/types';
-import { useEffect, useState } from 'react';
-import { useParams } from 'next/navigation';
-
-interface AwardItem {
-  id: string;
-  image: string;
-  name: string;
-  description: string;
-  productHeader: string;
-  productsByYear: ProductsByYear[];
-}
-
-interface ProductsByYear {
-  year: string;
-  products: ProductItem[];
-}
-
-interface ProductItem {
-  image: string;
-  name: string | null;
-  alt: string;
-  link: ButtonLinkElement;
-}
-
-
-
 
 const Zh = () => {
-
-    const { lang } = useParams();
-    const [awards, setAwards] = useState<AwardItem[]>([]);
-  
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_DEV_CMS_API_ENDPOINT}${process.env.NEXT_PUBLIC_DEV_CMS_ENDPOINT_SUFFIX}/${lang}/awards`);
-        const awardDatas = await response.json();
-        setAwards(awardDatas.data);
-        console.log(awardDatas);
-      } catch (error) {
-        console.log('\x1b[36m%s\x1b[0m', `==== DATA NOT FOUND () ====`);
-        console.error('error', error);
-      }
-    };
-
-    
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const hash = window.location.hash;
-      if (hash) {
-        const el = document.querySelector(hash);
-        if (el) {
-          el.scrollIntoView({ behavior: "smooth" });
-        }
-      }
-    }
-  }, [awards]);
-  
-    useEffect(() => {
-        fetchData();
-    }, []);
-    
   const scrollDirection = useScrollDirection();
 
   return (
@@ -76,7 +15,17 @@ const Zh = () => {
               <h1>
                 <a href='#body_inner'>DESIGN</a>
               </h1>
-              <AwardList data={awards} />
+              <ul>
+                <li>
+                  <a href='#iFDesign'>iF Design Award</a>
+                </li>
+                <li>
+                  <a href='#Reddot'>Red Dot Design Award</a>
+                </li>
+                <li>
+                  <a href='#Green'>Green Good Design Award</a>
+                </li>
+              </ul>
             </div>
           </section>
 
@@ -644,9 +593,8 @@ const Zh = () => {
                 </li>
               </ul>
             </section>
-            <Awards awards={awards} />
 
-            {/* <section className='block03 ttl award_wrap'>
+            <section className='block03 ttl award_wrap'>
               <p className='text10'>
                 Products created under the TOTO design philosophy have won prestigious international design awards.
               </p>
@@ -666,9 +614,9 @@ const Zh = () => {
                   <a href='#Green'>Green Good Design Award</a>
                 </li>
               </ul>
-            </section> */}
+            </section>
 
-            {/* <section className='block04'>
+            <section className='block04'>
               <article id='iFDesign' className='award_wrap'>
                 <h2>
                   <img src='/assets/global/award/img/if_logo.png' alt='iF Design Award' />
@@ -1281,7 +1229,7 @@ const Zh = () => {
                     </dl>
                   </div>
                 </div>
-              </article> */}
+              </article>
 
               {/* <article id="iFDesign_gold" className="award_wrap">
 						<h2><img src="/award/img/if_logo_gold.png" alt="iF Design Award" /></h2>
@@ -1298,7 +1246,7 @@ const Zh = () => {
 						</div>
 					</article> */}
 
-              {/* <article id='Reddot' className='award_wrap'>
+              <article id='Reddot' className='award_wrap'>
                 <h2>
                   <img src='/assets/global/award/img/rd_logo.png' alt='Reddot Design Award' />
                 </h2>
@@ -1399,14 +1347,14 @@ const Zh = () => {
                         <img src='/assets/global/award/img/202205/Stick_controler.png' alt='Stick controler' />
                       </dt>
                       <dd>Stick controler</dd>
-                    </dl> */}
+                    </dl>
 
                     {/* <dl>
 											<dt className="mb0"><img src="/award/img/202205/ONLY_Technology.png" alt="ONLY Technology PVD matte black" /></dt>
 											<dd className="caption_two">※Product Image</dd>
 											<dd>ONLY Technology<br/>"PVD matte black"</dd>
 										</dl> */}
-                  {/* </div>
+                  </div>
                 </div>
 
                 <div className='year'>
@@ -1962,7 +1910,7 @@ const Zh = () => {
                     </dl>
                   </div>
                 </div>
-              </article> */}
+              </article>
 
               {/* <article id="Reddot_bob" className="award_wrap">
 						<h2><img src="/award/img/rd_logo_bob_2023.png" alt="Reddot Design Award 2023"></h2>
@@ -2014,7 +1962,7 @@ const Zh = () => {
 
 					</article> */}
 
-              {/* <article id='Green' className='award_wrap'>
+              <article id='Green' className='award_wrap'>
                 <h2>
                   <img src='/assets/global/award/img/gg_logo.png' alt='Green Good Design Award' />
                 </h2>
@@ -2239,7 +2187,7 @@ const Zh = () => {
                 <br />
                 ・Products marked with * at the end are discontinued products.
               </p>
-            </section> */}
+            </section>
           </section>
 
           {/* <div className="pager"><ul id="pager"></ul></div> */}
